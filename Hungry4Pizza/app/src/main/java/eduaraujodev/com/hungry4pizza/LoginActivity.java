cf3a105bbd5765c501e88ddb56883da9c2d7c7ed
Eduardo Araujo <eduardo.araujo0@outlook.com>
2017-02-11 00:12:09 -0200

    Commit inicial do primeiro trabalho proposto pelo professor

---
+++ Hungry4Pizza/app/src/main/java/eduaraujodev/com/hungry4pizza/LoginActivity.java
@@ -0,0 +1,38 @@
+package eduaraujodev.com.hungry4pizza;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText etLogin;
+    private EditText etSenha;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etLogin = (EditText) findViewById(R.id.etLogin);
+        etSenha = (EditText) findViewById(R.id.etSenha);
+    }
+
+    public void logar(View view) {
+        String login = etLogin.getText().toString();
+        String senha = etSenha.getText().toString();
+
+        if (login.equals("FIAP") && senha.equals("123")) {
+            Intent intent = new Intent(this, PedidoActivity.class);
+
+            intent.putExtra("usuario", login);
+
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "Usuário ou senha invalido", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
