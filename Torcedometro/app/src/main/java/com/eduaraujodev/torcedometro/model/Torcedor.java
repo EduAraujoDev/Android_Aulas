708beff391068bce0baa7d6b498a6a531971690a
Eduardo Araujo <eduardo.araujo0@outlook.com>
2017-03-02 22:50:38 -0300

    Projeto da aula 4 - pdf

---
+++ Torcedometro/app/src/main/java/com/eduaraujodev/torcedometro/model/Torcedor.java
@@ -0,0 +1,31 @@
+package com.eduaraujodev.torcedometro.model;
+
+public class Torcedor {
+    private int id;
+    private String nome;
+    private Clube clube;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Clube getClube() {
+        return clube;
+    }
+
+    public void setClube(Clube clube) {
+        this.clube = clube;
+    }
+}
